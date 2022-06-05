51e490d50368bbaa4abdbb9aecca1a62a321acf8
hiepnguyen57 <hoahiepk10@gmail.com>
2022-06-05 17:09:10 +0700

    add adau1451 i2c driver

--- audiopacks/codecs/adau1451.c
+++ audiopacks/codecs/adau1451.c
@@ -15,10 +15,27 @@
 #include <sound/soc.h>
 
 #include "adau1451.h"
-
+#include "adau-utils.h"
 #define ADAU1451_FORMATS (SNDRV_PCM_FMTBIT_S16_LE | SNDRV_PCM_FMTBIT_S24_LE | \
 	SNDRV_PCM_FMTBIT_S32_LE)
 
+#define ADAU1451_SOFT_RESET ()
+
+static const struct reg_default adau1451_reg_defaults[] = {
+
+};
+
+struct adau1451_dai {
+	unsigned int clk_src;
+	unsigned int sysclk;
+	bool enable_src;
+	bool master;
+};
+
+struct adau1451{ 
+	struct regmap *regmap;
+	struct adau1451_dai dais[3];
+};
 
 static const struct snd_kcontrol_new adau1451_controls[] = {
 };
@@ -31,21 +48,37 @@ static const struct snd_soc_dapm_route adau1451_dapm_routes[] = {
 
 static int adau1451_probe(struct snd_soc_component *component) {
 
+	return 0;
 }
 
 static int adau1451_resume(struct snd_soc_component *component) {
 
+	return 0;
 }
 
 static int adau1451_set_bias_level(struct snd_soc_component *component,
 		enum snd_soc_bias_level level) {
 
+	return 0;
 }
 
 static int adau1451_set_pll(struct snd_soc_component *component, int pll_id,
 	int source, unsigned int freq_in, unsigned int freq_out) {
 
+	return 0;
 }
+
+static const struct remap_config adau1451_remap_config = {
+	.val_bits = 8,
+	.reg_bits = 8,
+
+	.volatile_reg = adau1451_register_volatile,
+	.max_register = ADAU1451_SOFT_RESET,
+
+	.cache_type = REGCACHE_RBTREE,
+	.reg_defaults = adau1451_reg_defaults,
+	.num_reg_defaults = ARRAY_SIZE(adau1451_reg_defaults),
+};
 
 static const struct snd_soc_component_driver adau1451_component_driver = {
 	.probe			= adau1451_probe,
@@ -81,6 +114,45 @@ static struct snd_soc_dai_driver adau1451_dai_driver = {
 	},
 	.ops = &adau1451_dai_ops,
 };
+
+static int adau1451_i2c_probe(struct i2c_client *client,
+                              const struct i2c_device_id *id) {
+	struct adau1451 *adau1451;
+	int ret;
+
+	adau1451 = devm_kzalloc(&client->dev, sizeof(*adau1451), GFP_KERNEL);
+	if (!adau1451) {
+		return -ENOMEM;
+	}
+
+	adau1451->regmap = devm_regmap_init_i2c(client,
+	&adau1451_remap_config);
+	if (IS_ERR(adau1451->regmap))
+		return PTR_ERR(adau1451->regmap);
+
+	regmap_write(adau1451->regmap, ADAU1451_SOFT_RESET, 0x00);
+	dev_set_drvdata(&client->dev, adau1451);
+	return devm_snd_soc_register_component(&client->dev,
+				&adau1451_component_driver,
+				adau1451_dai_driver, ARRAY_SIZE(adau1451_dai_driver));
+}
+
+static const struct i2c_device_id adau1451_i2c_id[] = {
+	{ "adau1451", 0 },
+	{ }
+};
+MODULE_DEVICE_TABLE(i2c, adau1451_i2c_id);
+
+
+static struct i2c_driver adau1451_i2c_driver = {
+	.driver = {
+		.name = "adau1451",
+	},
+	.probe = adau1451_i2c_probe,
+	.id_table = adau1451_i2c_id,
+};
+
+module_i2c_driver(adau1451_i2c_driver);
 
 MODULE_DESCRIPTION("ASoC ADAU1451 driver");
 MODULE_AUTHOR("Hiep Nguyen <hoahiepk10@gmail.com>");
