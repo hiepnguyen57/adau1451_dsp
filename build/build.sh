f3a3c7307fb497162e61330918e3ccadee2803a3
hiepnguyen57 <hoahiepk10@gmail.com>
2022-06-14 13:08:40 +0700

    - create audio defconfig - added adau1451 driver into codecs folder - config build

--- build/build.sh
+++ build/build.sh
@@ -5,14 +5,15 @@ KERNEL_SRC_DIR=${WORK_DIR}/linux-5.4.70
 MODULES_DIR="modules_imx"
 
 # Apply defconfig 
-cp ${WORK_DIR}/build/config ${KERNEL_SRC_DIR}/.config
+cp ${WORK_DIR}/build/config ${KERNEL_SRC_DIR}/arch/arm64/configs/audio_defconfig
 
 # Go to kernel source
 # Setup toolchains
-source build/env.sh
 cd ${KERNEL_SRC_DIR}
+source ../build/env.sh
 
 # Compile the kernel
+make audio_defconfig
 make -j$(nproc)
 
 # Compile the modules
---
+++ linux-5.4.70/sound/soc/codecs/adau1451.c
@@ -0,0 +1,202 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+//
+
+#define FORMAT(fmt) "%s: %d: " fmt, __func__, __LINE__
+#define pr_fmt(fmt) "ADAU1451" FORMAT(fmt)
+
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/delay.h>
+#include <linux/pm.h>
+#include <linux/i2c.h>
+#include <linux/slab.h>
+#include <linux/gcd.h>
+
+#include <sound/core.h>
+#include <sound/pcm.h>
+#include <sound/pcm_params.h>
+#include <sound/tlv.h>
+#include <sound/soc.h>
+
+#include "adau1451.h"
+#include "adau-utils.h"
+
+#define ADAU1451_FORMATS (SNDRV_PCM_FMTBIT_S16_LE | SNDRV_PCM_FMTBIT_S24_LE | \
+	SNDRV_PCM_FMTBIT_S32_LE)
+
+#define ADAU1451_SOFT_RESET (0x00)
+#define ADAU1451_ADC_DAC_STATUS (0x01)
+
+static const struct reg_default adau1451_reg_defaults[] = {
+
+};
+
+struct adau1451_dai {
+	unsigned int clk_src;
+	unsigned int sysclk;
+	bool enable_src;
+	bool master;
+};
+
+struct adau1451{ 
+	struct regmap *regmap;
+	struct adau1451_dai dais[3];
+};
+
+static const struct snd_kcontrol_new adau1451_controls[] = {
+};
+
+static const struct snd_soc_dapm_widget adau1451_dapm_widgets[] = {
+};
+
+static const struct snd_soc_dapm_route adau1451_dapm_routes[] = {
+};
+
+static int adau1451_probe(struct snd_soc_component *component) {
+	pr_debug("enter");
+	return 0;
+}
+
+static int adau1451_resume(struct snd_soc_component *component) {
+	pr_debug("enter");
+	return 0;
+}
+
+static int adau1451_set_bias_level(struct snd_soc_component *component,
+		enum snd_soc_bias_level level) {
+	pr_debug("enter");
+	return 0;
+}
+
+static int adau1451_set_pll(struct snd_soc_component *component, int pll_id,
+	int source, unsigned int freq_in, unsigned int freq_out) {
+	pr_debug("enter");
+	return 0;
+}
+
+static bool adau1451_register_volatile(struct device *dev, unsigned int reg) {
+	switch (reg) {
+	case ADAU1451_SOFT_RESET:
+	case ADAU1451_ADC_DAC_STATUS:
+		return true;
+	default:
+		return false;
+	}
+}
+
+static const struct regmap_config adau1451_remap_config = {
+	.val_bits = 8,
+	.reg_bits = 8,
+
+	.volatile_reg = adau1451_register_volatile,
+	.max_register = ADAU1451_SOFT_RESET,
+
+	.cache_type = REGCACHE_RBTREE,
+	.reg_defaults = adau1451_reg_defaults,
+	.num_reg_defaults = ARRAY_SIZE(adau1451_reg_defaults),
+};
+
+static const struct snd_soc_component_driver adau1451_component_driver = {
+	.probe			= adau1451_probe,
+	.resume			= adau1451_resume,
+	.set_bias_level		= adau1451_set_bias_level,
+	.set_pll		= adau1451_set_pll,
+	.controls		= adau1451_controls,
+	.num_controls		= ARRAY_SIZE(adau1451_controls),
+	.dapm_widgets		= adau1451_dapm_widgets,
+	.num_dapm_widgets	= ARRAY_SIZE(adau1451_dapm_widgets),
+	.dapm_routes		= adau1451_dapm_routes,
+	.num_dapm_routes	= ARRAY_SIZE(adau1451_dapm_routes),
+	.use_pmdown_time	= 1,
+	.endianness		= 1,
+	.non_legacy_dai_naming	= 1,
+};
+
+static int adau1451_hw_params(struct snd_pcm_substream *substream,
+	struct snd_pcm_hw_params *params, struct snd_soc_dai *dai) {
+	pr_debug("enter");
+	return 0;
+}
+
+static int adau1451_set_dai_fmt(struct snd_soc_dai *dai, unsigned int fmt) {
+	pr_debug("enter");
+	return 0;
+}
+
+static int adau1451_set_dai_sysclk(struct snd_soc_dai *dai,
+	int clk_id, unsigned int freq, int dir) {
+	pr_debug("enter");
+	return 0;
+}
+
+static const struct snd_soc_dai_ops adau1451_dai_ops = {
+	.hw_params	= adau1451_hw_params,
+	.set_sysclk	= adau1451_set_dai_sysclk,
+	.set_fmt	= adau1451_set_dai_fmt,
+};
+
+static struct snd_soc_dai_driver adau1451_dai_driver[] = {
+    {
+        .name = "adau-hifi",
+        .playback = {
+            .stream_name = "Playback",
+            .channels_min = 2,
+            .channels_max = 8,
+            .rates = SNDRV_PCM_RATE_8000_96000,
+            .formats = ADAU1451_FORMATS,
+        },
+        .capture = {
+            .stream_name = "Capture",
+            .channels_min = 2,
+            .channels_max = 8,
+            .rates = SNDRV_PCM_RATE_8000_96000,
+            .formats = ADAU1451_FORMATS,
+        },
+        .ops = &adau1451_dai_ops,
+    },
+};
+
+static int adau1451_i2c_probe(struct i2c_client *client,
+                              const struct i2c_device_id *id) {
+	struct adau1451 *adau1451;
+
+	pr_debug("enter");
+
+	adau1451 = devm_kzalloc(&client->dev, sizeof(*adau1451), GFP_KERNEL);
+	if (!adau1451) {
+		return -ENOMEM;
+	}
+
+	adau1451->regmap = devm_regmap_init_i2c(client,
+	&adau1451_remap_config);
+	if (IS_ERR(adau1451->regmap))
+		return PTR_ERR(adau1451->regmap);
+
+	regmap_write(adau1451->regmap, ADAU1451_SOFT_RESET, 0x00);
+	dev_set_drvdata(&client->dev, adau1451);
+	return devm_snd_soc_register_component(&client->dev,
+				&adau1451_component_driver,
+				adau1451_dai_driver,
+                ARRAY_SIZE(adau1451_dai_driver));
+}
+
+static const struct i2c_device_id adau1451_i2c_id[] = {
+	{ "adau1451", 0 },
+	{ }
+};
+MODULE_DEVICE_TABLE(i2c, adau1451_i2c_id);
+
+
+static struct i2c_driver adau1451_i2c_driver = {
+	.driver = {
+		.name = "adau1451",
+	},
+	.probe = adau1451_i2c_probe,
+	.id_table = adau1451_i2c_id,
+};
+
+module_i2c_driver(adau1451_i2c_driver);
+
+MODULE_DESCRIPTION("ASoC ADAU1451 driver");
+MODULE_AUTHOR("Hiep Nguyen <hoahiepk10@gmail.com>");
+MODULE_LICENSE("GPL");
